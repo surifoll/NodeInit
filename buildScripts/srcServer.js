920f4dd99d8fcfc1471e275c223047d6cb4f1759
Suraj <surifoll@yahoo.com>
2020-01-14 11:39:24 +0100

    server setup

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+
+const port = 3000;
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err) =>{
+    if (err) {
+        console.log(err);
+    }else{
+        open('http://localhost:'+ port)
+    }
+})
